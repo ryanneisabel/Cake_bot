2c6867a4346ab837fb924d2d4003362994295588
ryanneisabel <18113@my.sanctamaria.school.nz>
2023-06-22 14:56:58 +1200

    created component 1 and cake bot python file
    Created component 1 - Welcome Screen v1 and v2. Created cake bot python file.

---
+++ components/welcome_message v1.py
@@ -0,0 +1,16 @@
+import random
+from random import randint
+
+# list of random names
+names = ["Nadia","Alissa","Courtney","Mia","Emma","Jasper","Loui","Harry","Joe","Zachary"]
+
+num = randint(0,9)
+name = (names[num])
+
+print("== Welcome to Cloud Cakes ==")
+print("== My name is", name," ==")
+print("== I will be assisting you in picking cakes today! ==")
+
+
+
+
