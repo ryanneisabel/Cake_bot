0342fb97bc87a363406b21550a603f9c14bd436c
ryanneisabel <18113@my.sanctamaria.school.nz>
2023-07-17 12:02:46 +1200

    created component 4 - delivery information v1
    also made small edit to component 3 v3

--- components/delivery_information v1.py
+++ components/delivery_information v1.py
@@ -10,7 +10,6 @@ def not_blank(question):
         else:
             print("This cannot be blank ")
 
-
 # basic instructions
 question = ("Please enter your name ")
 customer_details['name'] = not_blank(question )
@@ -19,4 +18,20 @@ print(customer_details['name'])
 question = ("Please enter your phone number ")
 customer_details['phone'] = not_blank(question )
 print(customer_details['phone'])
+
+question = ("Please enter your house number ")
+customer_details['house'] = not_blank(question )
+print(customer_details['house'])
+
+
+question = ("Please enter your street name ")
+customer_details['street'] = not_blank(question )
+print(customer_details['street'])
+
+
+question = ("Please enter your suburb ")
+customer_details['suburb'] = not_blank(question )
+print(customer_details['suburb'])
+
+
 
