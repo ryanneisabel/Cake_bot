b87ec591a8243af09784045902d1d172e633b965
ryanneisabel <18113@my.sanctamaria.school.nz>
2023-07-25 15:30:08 +1200

    created component 13 - phone number validator v1

---
+++ components/val_phone v1.py
@@ -0,0 +1,22 @@
+question = ("Please enter your phone number ")
+
+PH_LOW = 7
+PH_HIGH = 10
+
+while True:
+    try:
+        num = int(input(question))
+        test_num = num
+        count = 0 
+        while test_num > 0: 
+            test_num = test_num//10
+            count = count + 1
+        if count >= PH_LOW and count <= PH_HIGH:
+            print(num)
+            break
+        else:
+            print("NZ phone numbers have between 7 and 10 digits")
+    except ValueError:
+        print("Please enter a number ")
+
+        
